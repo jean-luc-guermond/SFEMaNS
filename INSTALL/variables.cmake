9d0ac4d883f6dffcb69ac799d55933d5f110db38
MelvinCreff <ufz17qe@jean-zay3.idris.fr>
2022-01-11 20:22:32 +0100

    Changed CMake for portability and Jean-Zay

---
+++ INSTALL/variables.cmake
@@ -0,0 +1,13 @@
+# Variables to set for Jean-Zay
+set(ENV{CC} "icc")
+set(ENV{CXX} "icpc")
+set(ENV{FC} "ifort")
+set(SFEMaNS_DIR "/gpfswork/rech/nor/commun/SFEMaNS_v5.4/SFEMaNS")
+set(COMPILE_OPTIONS "-fpp")
+# Variables to set for Whistler
+#set(ENV{CC} "icc")
+#set(ENV{CXX} "icpc")
+#set(ENV{FC} "ifort")
+#set(SFEMaNS_DIR "/gpfswork/rech/nor/commun/SFEMaNS_v5.4/SFEMaNS")
+#set(COMPILE_OPTIONS "-fpp")
+
