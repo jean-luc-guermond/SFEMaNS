70b4c6c67474d46256806b3662918a543b367dd5
Melvin Creff <creff@limsi.fr>
2022-01-14 16:27:01 -0600

    Re organizing.

--- TEMPLATE/variables_template.cmake
+++ TEMPLATE/variables_template.cmake
@@ -3,8 +3,8 @@
 #set(ENV{CXX} "icpc")
 #set(ENV{FC} "ifort")
 #set(SFEMaNS_DIR "/gpfswork/rech/nor/commun/SFEMaNS_v5.4/SFEMaNS")
-
-
+#set(debug_bounds "$ENV(FFLAGS) -O2 -g -traceback -heap-arrays")
+#set(release_bounds "-O3")
 
 # Variables to set for Whistler
 set(SFEMaNS_DIR "/home/guermond/SFEMaNS/GIT_SFEMaNS/SFEMaNS")
