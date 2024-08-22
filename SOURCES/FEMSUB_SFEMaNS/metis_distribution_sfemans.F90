716a2e70dee315d7bc8e289135c9c8cc824a5c50
mcreff <melvin.creff@gmail.com>
2024-08-22 14:49:15 -0500

    mc

--- SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
+++ SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
@@ -1598,8 +1598,12 @@ CONTAINS
       !==We create the local mesh now
       mesh%edge_stab = .FALSE.
       WRITE(*, *)'11'
+      WRITE(*,*) 'g medge', mesh%medge, 'np ', mesh%np, 'me ', mesh%me, 'mes ', mesh%mes, 'nps ', mesh%nps
 
       CALL create_local_mesh_with_extra_layer(mesh, mesh_loc, me_loc, mes_loc, np_loc)
+            WRITE(*,*) 'l medge', mesh_loc%medge, 'np ', mesh_loc%np, 'me ', mesh_loc%me, 'mes ', mesh_loc%mes, &
+                 'nps ', mesh_loc%nps
+
       WRITE(*, *)'12'
 
       CALL free_mesh(mesh)
@@ -1779,11 +1783,7 @@ CONTAINS
 
       !==We create the local mesh now
       mesh%edge_stab = .FALSE.
-      WRITE(*,*) 'g medge', mesh%medge, 'np ', mesh%np, 'me ', mesh%me, 'mes ', mesh%mes, 'nps ', mesh%nps
-
       CALL create_local_mesh(mesh, mesh_loc, me_loc, mes_loc, np_loc)
-      WRITE(*,*) 'l medge', mesh_loc%medge, 'np ', mesh_loc%np, 'me ', mesh_loc%me, 'mes ', &
-           mesh_loc%mes, 'nps ', mesh_loc%nps
 
       DEALLOCATE(list_m, tab, tabs)
 
