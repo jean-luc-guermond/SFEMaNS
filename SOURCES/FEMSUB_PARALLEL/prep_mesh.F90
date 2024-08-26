9c1b9e56bb85398d4fec35354e7f1bbdd0d8817d
mcreff <melvin.creff@gmail.com>
2024-08-26 11:09:08 -0500

    mc

--- SOURCES/FEMSUB_PARALLEL/prep_mesh.F90
+++ SOURCES/FEMSUB_PARALLEL/prep_mesh.F90
@@ -1906,7 +1906,6 @@ CONTAINS
       END DO
 
       !===GENERATION OF THE Pk GRID
-      WRITE(*,*) mesh%np, mesh_p1%np, mesh_p1%medge, mesh_p1%medges
       mesh%rr(:, 1:dom_np) = mesh_p1%rr(:, 1:dom_np)
       mesh%rr(:, mesh%dom_np + 1:mesh%dom_np + np - dom_np) = mesh_p1%rr(:, dom_np + 1:)
       mesh%jj(1:nw, :) = mesh_p1%jj
