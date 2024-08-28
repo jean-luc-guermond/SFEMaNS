3e240cb36ebede85058d422b4d7ddc637d1bc5a8
mcreff <melvin.creff@gmail.com>
2024-08-28 10:57:06 -0500

    mc

--- SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
+++ SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
@@ -2086,7 +2086,7 @@ CONTAINS
 
       CALL MPI_ALLGATHER(mesh_loc%dom_np, 1, MPI_INTEGER, mesh_loc%domnp, 1, &
            MPI_INTEGER, PETSC_COMM_WORLD, ierr)
-      write(*,*) 'p1 domnp', mesh_loc%domnp, dom_np
+      write(*,*) 'p1 domnp', mesh_loc%domnp, dom_np, np_loc(2), np_loc(1)
       mesh_loc%disp(1) = 1
       DO n = 1, nb_proc
          mesh_loc%disp(n + 1) = mesh_loc%disp(n) + mesh_loc%domnp(n)
