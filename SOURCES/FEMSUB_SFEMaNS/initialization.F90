69113a75ad9c9e2da4b278aad066e16aaafd1e89
mcreff <melvin.creff@gmail.com>
2024-08-28 10:18:55 -0500

    mc

--- SOURCES/FEMSUB_SFEMaNS/initialization.F90
+++ SOURCES/FEMSUB_SFEMaNS/initialization.F90
@@ -1107,7 +1107,7 @@ CONTAINS
             CALL st_aij_csr_glob_block_with_extra_layer(comm_one_d_ns(1), 1, vv_mesh, vizu_rot_u_LA)
          END IF
 
-         write(*,*) 'np', vv_mesh%disp, 'LA', SIZE(vv_3_LA%ia), 'm', MAXVAL(vv_mesh%loc_to_glob(:))
+         write(*,*) 'np', vv_mesh%disp, 'LA', SIZE(vv_3_LA%ia), 'm', MAXVAL(vv_3_LA%ja)
 
 
          !TODO ===Create symmetric points==================================================
