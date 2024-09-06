c3a447325ca52afa7529c195562459544cf192dc
mcreff <melvin.creff@gmail.com>
2024-09-06 16:32:29 -0500

    mc

--- SOURCES/FEMSUB_OBJET/prep_mesh_interface.F90
+++ SOURCES/FEMSUB_OBJET/prep_mesh_interface.F90
@@ -182,6 +182,7 @@ CONTAINS
                   n2_ks = (/MODULO(k2 + 1, nw) + 1, MODULO(k2, nw) + 1/)
                END IF
                write(*,*) k2, n2_ks, k1, n1_ks
+
                IF (MAXVAL(ABS(mesh_master%rrs_extra(:, n1_ks, ms1) - mesh_slave%rrs_extra(:, n2_ks, ms2))).GT.epsilon) THEN
                   write(*,*)  'n1' ,mesh_master%rrs_extra(:, n1_ks, ms1)
                   write(*,*) 'n2' , mesh_slave%rrs_extra(:, n2_ks, ms2)
@@ -190,11 +191,15 @@ CONTAINS
 
                IF(.NOT.virgin_elem(ms2)) THEN
                   Okay = .TRUE.
+                  write(*,*) '???'
                   CYCLE !element already done
                ENDIF
 
                r_norm = SUM(ABS(mesh_master%rrs_extra(:, 1:3, ms1)) - mesh_slave%rrs_extra(:, 1:3, ms2))
-               IF (r_norm .LE. 1d-9) CYCLE ! two identical triangles
+               IF (r_norm .LE. 1d-9) THEN
+                  write(*,*) '?????'
+                  CYCLE
+               END IF ! two identical triangles
 
                ms = ms + 1
                interface_mesh1(ms) = ms1
