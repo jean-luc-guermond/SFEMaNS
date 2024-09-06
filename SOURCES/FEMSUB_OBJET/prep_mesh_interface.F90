b94f9ce9de26202ad068636d5ed93bab47da6bc1
mcreff <melvin.creff@gmail.com>
2024-09-06 15:58:53 -0500

    mc

--- SOURCES/FEMSUB_OBJET/prep_mesh_interface.F90
+++ SOURCES/FEMSUB_OBJET/prep_mesh_interface.F90
@@ -211,10 +211,8 @@ CONTAINS
 
       mesh_interface%mes_extra = ms
       write(*, *) 'm1', ms, mesh_master%mes_extra, mesh_master%mextra
-      write(*, *) 'jcc', mesh_master%jcc_extra
-      write(*, *) 'jneighs', mesh_master%neighs_extra
-      write(*, *) 'jcc s', mesh_slave%jcc_extra
-      write(*, *) 'jneighs s', mesh_slave%neighs_extra
+      write(*, *) 'jcc', mesh_master%neighs_extra
+      write(*, *) 'side', mesh_master%sides_extra
       ALLOCATE(mesh_interface%mesh1(ms), mesh_interface%mesh2(ms), &
            mesh_interface%jjs1(nws_master, ms), mesh_interface%jjs2(nws_slave, ms))
       IF (ms > 0) THEN
