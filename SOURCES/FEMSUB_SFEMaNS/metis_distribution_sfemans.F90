bba3e1fd8dda7c366808251cb904b3edb0553d9b
mcreff <melvin.creff@gmail.com>
2024-09-16 14:50:54 -0500

    mc

--- SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
+++ SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
@@ -2359,7 +2359,7 @@ CONTAINS
             virginss(m) = .FALSE.
             IF (MINVAL(ABS(mesh%neighs - m)) == 0) THEN
                CALL find_cell_interface(mesh, m, m2)
-               IF (m2 > -1 .AND. virginss(m2)) THEN
+               IF (m2 > 0 .AND. virginss(m2)) THEN
                   nb_extra = nb_extra + 1
                   mesh_loc%jj_extra(:, nb_extra) = mesh%jj(:, m2)
                   mesh_loc%jce_extra(:, nb_extra) = mesh%jce(:, m2)
