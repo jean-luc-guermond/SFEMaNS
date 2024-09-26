fbc82dbb64c995c1cfcdfef071274f203acc029f
mcreff <melvin.creff@gmail.com>
2024-09-26 15:29:43 -0500

    :qmes

--- SOURCES/FEMSUB_SFEMaNS/restart.F90
+++ SOURCES/FEMSUB_SFEMaNS/restart.F90
@@ -275,8 +275,8 @@ CONTAINS
           IF (mode == mode_cherche) THEN   !on a trouve le bon mode
              WRITE(*,*) 'mode', rank , mode, list_mode, SIZE(un(:,1,1)), SIZE(un(1,:,1))
              IF (rank == 1) THEN
-             READ(10) un(:,1:1,i)
-             write(*,*)  'un'
+             READ(10) un(:,:,i)
+             write(*,*)  'un ??????????'
              READ(10) un_m1(:,:,i)
                           write(*,*)  'un_m1'
              READ(10) pn(:,:,i)
