284ba515c6381860a2c158825ee88dde288a4805
mcreff <melvin.creff@gmail.com>
2024-09-26 14:59:40 -0500

    mes

--- SOURCES/FEMSUB_SFEMaNS/restart.F90
+++ SOURCES/FEMSUB_SFEMaNS/restart.F90
@@ -270,9 +270,10 @@ CONTAINS
                 mode_cherche = mode
              END IF
           END IF
-          WRITE(*,*) 'mode', mode, list_mode, SIZE(un(:,1,1)), SIZE(un(1,:,1))
+
           !June 7 2007, JLG
           IF (mode == mode_cherche) THEN   !on a trouve le bon mode
+             WRITE(*,*) 'mode', mode, list_mode, SIZE(un(:,1,1)), SIZE(un(1,:,1))
              READ(10) un(:,:,i)
              READ(10) un_m1(:,:,i)
              READ(10) pn(:,:,i)
