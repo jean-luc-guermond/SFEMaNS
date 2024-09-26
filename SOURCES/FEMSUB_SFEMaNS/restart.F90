7ffbf0c06e8646f308a34719abef225438636bb6
mcreff <melvin.creff@gmail.com>
2024-09-26 15:24:05 -0500

    :qmes

--- SOURCES/FEMSUB_SFEMaNS/restart.F90
+++ SOURCES/FEMSUB_SFEMaNS/restart.F90
@@ -273,15 +273,7 @@ CONTAINS
 
           !June 7 2007, JLG
           IF (mode == mode_cherche) THEN   !on a trouve le bon mode
-             WRITE(*,*) 'mode', mode, list_mode, SIZE(un(:,1,1)), SIZE(un(1,:,1))
-             IF (rank == 0) THEN
-                DO n = 1, SIZE(un(:,1,1))
-                   DO k = 1, SIZE(un(1,:,1))
-                     READ(10) un(:,1,1)
-                     WRITE(*,*) n, k, un(:,1:6,1)
-                   END DO
-                END DO
-             END IF
+             WRITE(*,*) 'mode', rank , mode, list_mode, SIZE(un(:,1,1)), SIZE(un(1,:,1))
              READ(10) un(:,:,i)
              READ(10) un_m1(:,:,i)
              READ(10) pn(:,:,i)
