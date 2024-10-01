0a30a59437be677d1e7b659adf4224bd498f138d
mcreff <melvin.creff@gmail.com>
2024-10-01 13:43:08 -0500

    test

--- TEMPLATE/main.F90
+++ TEMPLATE/main.F90
@@ -104,9 +104,13 @@ PROGRAM mhd_prog
   DO it = 1, inputs%nb_iteration
      tploc =  user_time()
      time = time + inputs%dt
-
+     IF (i ==  1) THEN
+         CALL plot_scalar_field(vv_mesh%jj, vv_mesh%rr, un(:, 1), 'uui.plt')
+     END IF
      CALL run_SFEMaNS(time, it)
-
+     IF (i ==  inputs%nb_iteration) THEN
+         CALL plot_scalar_field(vv_mesh%jj, vv_mesh%rr, un(:, 1), 'uuf.plt')
+     END IF
      !===My postprocessing
      IF (.NOT.inputs%test_de_convergence) THEN
         CALL my_post_processing(it)
