c5ef7a2e69428ddfd3fa49db67ede7f8039b8e37
mcreff <melvin.creff@gmail.com>
2024-10-09 14:05:55 -0500

    test

--- SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
+++ SOURCES/FEMSUB_SFEMaNS/metis_distribution_sfemans.F90
@@ -2511,7 +2511,7 @@ CONTAINS
          part(mesh%neighs) = mesh%sides
          part(mesh_loc%neighs_extra) = -3.d0
          part(188+489) = -4.d0
-         write(*, *) 'dd', count, mesh%jj(:, 188)
+         write(*, *) 'dd', count, mesh%jj(:, 188+489)
          WRITE(tit, '(i1)') count
          CALL plot_const_p1_label(mesh%jj, mesh%rr, 1.d0 * part, tit // 'dd.plt')
       END IF
