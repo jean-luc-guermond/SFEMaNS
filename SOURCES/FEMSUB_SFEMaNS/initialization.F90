65923d7dc54f370f1f4e0a3847259f66af0ae204
mcreff <melvin.creff@gmail.com>
2024-10-28 14:26:45 -0500

    test

--- SOURCES/FEMSUB_SFEMaNS/initialization.F90
+++ SOURCES/FEMSUB_SFEMaNS/initialization.F90
@@ -1223,7 +1223,8 @@ CONTAINS
 
       !===Specific to induction equation==============================================
       IF (if_induction) THEN
-
+            IF (rank == 0) CALL plot_const_p1_label(vv_mesh%jj, vv_mesh%rr, 1.d0 * vv_mesh%jj(1,:) , 'vv.plt')
+            IF (rank == 0) CALL plot_const_p1_label(H_mesh%jj, H_mesh%rr, 1.d0 * H_mesh%jj(1,:), 'HH.plt')
          !===Verify that pmag_mesh and H_mesh coincide================================
          IF (pmag_mesh%me/=0) THEN
             error = 0.d0
@@ -1283,8 +1284,6 @@ CONTAINS
 
          !===Verify if H and NS meshes coincide on the NS domain=======================
          IF (if_momentum .OR. inputs%type_pb=='mxx') THEN
-            IF (rank == 0) CALL plot_const_p1_label(vv_mesh%jj, vv_mesh%rr, 1.d0 * vv_mesh%jj(1,:) , 'vv.plt')
-            IF (rank == 0) CALL plot_const_p1_label(H_mesh%jj, H_mesh%rr, 1.d0 * H_mesh%jj(1,:), 'HH.plt')
             IF (vv_mesh%me/=0) THEN
                error = 0.d0
                DO k = 1, 2
