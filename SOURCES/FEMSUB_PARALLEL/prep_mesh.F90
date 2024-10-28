70259e953d002dc1763f7532b17b1c74b62c135e
mcreff <melvin.creff@gmail.com>
2024-10-28 12:21:54 -0500

    test

--- SOURCES/FEMSUB_PARALLEL/prep_mesh.F90
+++ SOURCES/FEMSUB_PARALLEL/prep_mesh.F90
@@ -2073,7 +2073,6 @@ CONTAINS
                      EXIT
                   END IF
                ENDDO
-               write(*,*) 'mm', m_op_k, m
                DO l = 1, f_dof
                   j_mid((k - 1) * f_dof + l, m) = j_mid((kk - 1) * f_dof + l, mm) !===New index created
                END DO
