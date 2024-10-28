599d2c62ec49c554a6d8a1d68c6b14905093efa7
mcreff <melvin.creff@gmail.com>
2024-10-28 13:31:27 -0500

    test

--- SOURCES/FEMSUB_SFEMaNS/initialization.F90
+++ SOURCES/FEMSUB_SFEMaNS/initialization.F90
@@ -1313,18 +1313,13 @@ CONTAINS
                     + MAXVAL(ABS(vv_mesh%rr(2, vv_mesh%jj(6, :)) &
                          - (H_mesh%rr(2, H_mesh%jj(1, 1:vv_mesh%me)) + H_mesh%rr(2, H_mesh%jj(2, 1:vv_mesh%me))) / 2))
                DO m = 1, vv_mesh%me
-                  write(*,*) ABS(vv_mesh%rr(1, vv_mesh%jj(4, m)) &
-                    - (H_mesh%rr(1, H_mesh%jj(2, m)) + H_mesh%rr(1, H_mesh%jj(3, m))) / 2),&
-                    + ABS(vv_mesh%rr(1, vv_mesh%jj(5, m)) &
-                         - (H_mesh%rr(1, H_mesh%jj(3, m)) + H_mesh%rr(1, H_mesh%jj(1, m))) / 2),&
-                    + ABS(vv_mesh%rr(1, vv_mesh%jj(6, m)) &
-                         - (H_mesh%rr(1, H_mesh%jj(1, m)) + H_mesh%rr(1, H_mesh%jj(2, m))) / 2),&
-                    + ABS(vv_mesh%rr(2, vv_mesh%jj(4, m)) &
-                         - (H_mesh%rr(2, H_mesh%jj(2, m)) + H_mesh%rr(2, H_mesh%jj(3, m))) / 2),&
-                    + ABS(vv_mesh%rr(2, vv_mesh%jj(5, m)) &
-                         - (H_mesh%rr(2, H_mesh%jj(3, m)) + H_mesh%rr(2, H_mesh%jj(1, m))) / 2),&
-                    + ABS(vv_mesh%rr(2, vv_mesh%jj(6, m)) &
-                         - (H_mesh%rr(2, H_mesh%jj(1, m)) + H_mesh%rr(2, H_mesh%jj(2, m))) / 2), m, vv_mesh%neigh(:, m)
+                  write(*,*) vv_mesh%rr(1, vv_mesh%jj(1:3, m)) - H_mesh%rr(1, H_mesh%jj(1:3, m),&
+                  vv_mesh%rr(1, vv_mesh%jj(4, m)) &
+                    - (H_mesh%rr(1, H_mesh%jj(2, m)) + H_mesh%rr(1, H_mesh%jj(3, m))) / 2,&
+                    + vv_mesh%rr(1, vv_mesh%jj(5, m)) &
+                         - (H_mesh%rr(1, H_mesh%jj(3, m)) + H_mesh%rr(1, H_mesh%jj(1, m))) / 2,&
+                    + vv_mesh%rr(1, vv_mesh%jj(6, m)) &
+                         - (H_mesh%rr(1, H_mesh%jj(1, m)) + H_mesh%rr(1, H_mesh%jj(2, m))) / 2, m, vv_mesh%neigh(:, m)
                END DO
                write(*,*) vv_mesh%rr(1,vv_mesh%jj(1, :)) - H_mesh%rr(1, H_mesh%jj(1, 1:vv_mesh%me))
                IF (error / MAXVAL(ABS(H_mesh%rr(1, 1) - H_mesh%rr(1, :))) .GE. 5.d-14) THEN
